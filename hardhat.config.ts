39981c031cfa62da1e18d56099fd954e669188d4
CJ <CjHare@users.noreply.github.com>
2021-09-27 18:48:59 +1000

    chore: alphabetical ordering

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,19 +7,19 @@ import {log} from './config/logging'
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners()
+    const accounts = await hre.ethers.getSigners()
 
-  log.info('List of available Accounts')
-  for (const account of accounts) {
-    log.info('%s', account.address)
-  }
+    log.info('List of available Accounts')
+    for (const account of accounts) {
+        log.info('%s', account.address)
+    }
 })
 
 // task action function receives the Hardhat Runtime Environment as second argument
 task('blockNumber', 'Prints the current block number', async (_, {ethers}) => {
-  await ethers.provider.getBlockNumber().then((blockNumber) => {
-    log.info('Current block number: %d', blockNumber)
-  })
+    await ethers.provider.getBlockNumber().then((blockNumber) => {
+        log.info('Current block number: %d', blockNumber)
+    })
 })
 
 // You need to export an object to set up your config
@@ -27,5 +27,5 @@ task('blockNumber', 'Prints the current block number', async (_, {ethers}) => {
 
 // Last Solidity for 0.7.x stream is 0.7.6
 export default {
-  solidity: '0.7.6'
+    solidity: '0.7.6'
 }
