22bae6c422c1bc846175fb1efd0abebabaf9c01a
Red Cyrax <cyraxred@users.noreply.github.com>
2022-08-24 15:21:47 +0400

    Use dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,8 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@openzeppelin/hardhat-upgrades'
 import '@nomiclabs/hardhat-waffle'
+import 'hardhat-contract-tools'
 import {log} from './config/logging'
-import './plugins/contract-sizes'
 
 /*
  * This is a sample Hardhat task. To learn how to create your own go to https://hardhat.org/guides/create-task.html
